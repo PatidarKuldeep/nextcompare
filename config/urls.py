d8cd62e25e69865bccc061e70868cafa7bf5dc96
PatidarKuldeep <kandypatel93@gmail.com>
2026-03-06 18:28:38 +0530

    updated comparison page and fixes

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,7 @@ urlpatterns = [
     "robots.txt",
     TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
-    path(
-    "sitemap.xml",
+    path("sitemap.xml",
     sitemap,
     {"sitemaps": sitemaps},
     name="sitemap",
