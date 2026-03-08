8b56f825a672638e3bd805cbc6a92abdc2fe6a74
PatidarKuldeep <kandypatel93@gmail.com>
2026-03-08 19:52:08 +0530

    fixed processor import in views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, processor
 from datetime import date, timedelta
 from django.db.models import Q
 from django.http import JsonResponse
